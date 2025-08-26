78fdc46dd7b9131af7138324b54b7b9cadc7106e
andrespipe94 <afbermudez151@gmail.com>
2025-08-26 15:30:21 -0500

    agregar y limpiar

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-let amigos = []
+let listaDeAmigos = []; 
+
+function agregarAmigo() {
+    
+    let amigo = document.getElementById('amigo').value;
+
+    
+    if (amigo === "") {
+        alert("Por favor, inserte un nombre");
+        return;
+    }
+    else {
+        
+        listaDeAmigos.push(amigo);
+
+        
+        limpiar();
+
+        
+        actualizarLista();
+        
+        
+       document.getElementById('reset').disabled = false;
+    }
+}
+
+function actualizarLista() {
+    let amigo = document.getElementById('listaAmigos'); 
+
+    amigo.innerHTML = ''; 
+
+    for (let i = 0; i < listaDeAmigos.length; i++) { 
+        let nombre = listaDeAmigos[i]; 
+        let li = document.createElement('li'); 
+
+        li.innerHTML = nombre; 
+        amigo.appendChild(li); 
+    }
+}
+
+function limpiar() {
+     document.getElementById('amigo').value = '';
+}
